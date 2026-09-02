19c620e83723784ccb521d7d9b39b0729bba7f44
Asif Hussain Hurrah <282775435+AasifHussain12@users.noreply.github.com>
2026-09-02 09:55:22 +0530

    Add variable declarations and print statements in Python

---
+++ 02_variables.py
@@ -0,0 +1,22 @@
+# Variables in Python
+
+name = "Aasif Hussain"
+age = 24
+course = "MSc IT"
+
+print("Student Information")
+print("Name:", name)
+print("Age:", age)
+print("Course:", course)
+
+# Integer
+marks = 95
+print("\nMarks:", marks)
+
+# Float
+cgpa = 8.20
+print("CGPA:", cgpa)
+
+# Boolean
+is_student = True
+print("Is Student:", is_student)
