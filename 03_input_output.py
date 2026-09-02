973fa1c22026dc0b76d041fb4fd5b0e03f0763ff
Asif Hussain Hurrah <282775435+AasifHussain12@users.noreply.github.com>
2026-09-02 12:49:13 +0530

    Add input and output example in Python

---
+++ 03_input_output.py
@@ -0,0 +1,14 @@
+# Input and Output in Python
+
+print("Student Information")
+
+# Taking input from the user
+name = input("Enter your name:")
+age = input("Enter your age:")
+course = input("Enter your course:")
+
+# Displaying output
+print("\nDetails Entered")
+print("Name   :", name)
+print("Age    :", age)
+print("Course :", course)
